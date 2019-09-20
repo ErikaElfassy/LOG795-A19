10a40f5911dc58bcff26af59b04e879883f32eba
Matthieu Pilon <pilon.matthieu@gmail.com>
2019-09-20 17:27:10 -0400

    State Machine Ready for tests

--- PluginDevelopement/Plugins/StateMachine/Source/StateMachine/Private/StateMachine.cpp
+++ PluginDevelopement/Plugins/StateMachine/Source/StateMachine/Private/StateMachine.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
 #include "StateMachine.h"
 
@@ -7,6 +7,7 @@
 void FStateMachineModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	UE_LOG(LogTemp, Warning, TEXT("State Machine Module loaded!"));
 }
 
 void FStateMachineModule::ShutdownModule()
